a87404ee017155a29bff5931ddf3ee6fe53a46da
thelaw51 <joegillard3@gmail.com>
2022-03-17 17:06:29 +0000

    Got display of the day of the week working and the placement of the text correct

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 class DigitalClock {
   constructor(element) {
     this.element = element;
-    console.log(this.element);
   }
 
   start() {
@@ -17,6 +16,36 @@ class DigitalClock {
     const secondsFormatted = parts.seconds.toString().padStart(2, "0");
     const timeFormatted = `${parts.hour}:${minuteFormatted}:${secondsFormatted}`;
     const amPm = parts.isAm ? "AM" : "PM";
+    const day = parts.day;
+    switch (day) {
+      case 0:
+        const today0 = document.querySelector(".Sunday");
+        today0.style.color = "#00FF97";
+        break;
+      case 1:
+        const today1 = document.querySelector(".Monday");
+        today1.style.color = "#00FF97";
+        break;
+      case 2:
+        const today2 = document.querySelector(".Tuesday");
+        today2.style.color = "#00FF97";
+        break;
+      case 3:
+        const today3 = document.querySelector(".Wednesday");
+        today3.style.color = "#00FF97";
+        break;
+      case 4:
+        const today4 = document.querySelector(".Thursday");
+        today4.style.color = "#00FF97";
+        break;
+      case 5:
+        const today5 = document.querySelector(".Friday");
+        today5.style.color = "#00FF97";
+        break;
+      case 6:
+        const today6 = document.querySelector(".Saturday");
+        today6.style.color = "#00FF97";
+    }
 
     this.element.querySelector(".clock-time").textContent = timeFormatted;
     this.element.querySelector(".clock-amPm").textContent = amPm;
@@ -29,6 +58,7 @@ class DigitalClock {
       minute: now.getMinutes(),
       seconds: now.getSeconds(),
       isAm: now.getHours() < 12,
+      day: now.getDay(),
     };
   }
 }
@@ -36,4 +66,5 @@ class DigitalClock {
 const clockElement = document.querySelector(".clock");
 const clockObject = new DigitalClock(clockElement);
 
+console.log();
 clockObject.start();
