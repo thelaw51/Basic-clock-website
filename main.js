de3e66e7852abc4cc742e64d5f449cc8cca919c7
thelaw51 <joegillard3@gmail.com>
2022-05-08 11:56:02 +0100

    more attempts to figure out 24 hour format still unsuccessful

--- main.js
+++ main.js
@@ -12,11 +12,10 @@ class DigitalClock {
 
   update() {
     const parts = this.getTimeParts();
-    const twelveHourMinuteFormatted = parts.minute.toString().padStart(2, "0");
-    const twelveHoursecondsFormatted = parts.seconds
-      .toString()
-      .padStart(2, "0");
-    const timeFormatted = `${parts.hour}:${twelveHourMinuteFormatted}:${twelveHoursecondsFormatted}`;
+    const MinuteFormatted = parts.minute.toString().padStart(2, "0");
+    const secondsFormatted = parts.seconds.toString().padStart(2, "0");
+    const twelvehourtimeFormatted = `${parts.twelvehour}:${MinuteFormatted}:${secondsFormatted}`;
+    const twentyfourhourtimeFormatted = `${parts.twentyfourhour}:${MinuteFormatted}:${secondsFormatted}`;
     const amPm = parts.isAm ? "AM" : "PM";
     const day = parts.day;
     switch (day) {
@@ -48,14 +47,16 @@ class DigitalClock {
         const today6 = document.querySelector(".Saturday");
         today6.style.color = "#00FF97";
     }
-    this.element.querySelector(".clock-time").textContent = timeFormatted;
+    this.element.querySelector(".clock-time").textContent =
+      twelvehourtimeFormatted;
     this.element.querySelector(".clock-amPm").textContent = amPm;
   }
 
   getTimeParts() {
     const now = new Date();
     return {
-      hour: now.getHours() % 12 || 12,
+      twelvehour: now.getHours() % 12 || 12,
+      twentyfourhour: now.getHours(),
       minute: now.getMinutes(),
       seconds: now.getSeconds(),
       isAm: now.getHours() < 12,
@@ -70,14 +71,13 @@ const clockObject = new DigitalClock(clockElement);
 console.log();
 clockObject.start();
 
-function timeFormatTwentyFourHours(clockElement) {
+function timeFormatTwentyFourHours() {
   const amPmDisplay = document.querySelector(".clock-amPm");
   amPmDisplay.style.visibility = "hidden";
   const clockRecenter = document.querySelector(".clock");
   clockRecenter.style.left = "55%";
   const clockRecenter2 = document.querySelector(".clock");
   clockRecenter2.style.top = "60%";
-  const now = new Date();
 }
 function timeFormatTwelveHour() {
   const amPmDisplay = document.querySelector(".clock-amPm");
