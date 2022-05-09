ef82b503e3936bb4e33befa0f0d7ddfaf25d5d1f
thelaw51 <joegillard3@gmail.com>
2022-05-09 12:08:19 +0100

    removing the changing of time formats fuctionality would rather have the finished product then have a project that i never finish also added comments to help with learning javascript

--- main.js
+++ main.js
@@ -2,22 +2,30 @@ class DigitalClock {
   constructor(element) {
     this.element = element;
   }
-
+  //function to Infinity call the update fuction to update the time on the clock every half a second.
   start() {
+    //runs the update function as soon as the webpage loads to make sure there is no moment before the function is run.
     this.update();
+    //runs the update function infinity every 500 miliseconds
     setInterval(() => {
       this.update();
     }, 500);
   }
-
+  //function to update the clock and use the get time parts fuctions returned values to update the on screen cloc and decide if it is am or pm and what day of the week it is.
   update() {
+    // bringing in the returned values from the get time parts function to format for updating the clock on the website.
     const parts = this.getTimeParts();
+    // constant for correctly formatting the minutes so they can be displayed correctly later adding a 0 placeholder if the minutes are a single digit
     const MinuteFormatted = parts.minute.toString().padStart(2, "0");
+    // constant for correctly formatting the seconds so they can be displayed correctly later adding a 0 placeholder if the seconds are a single digit
     const secondsFormatted = parts.seconds.toString().padStart(2, "0");
-    const twelveHourTimeFormatted = `${parts.twelvehour}:${MinuteFormatted}:${secondsFormatted}`;
-    const twentyFourHourTimeFormatted = `${parts.twentyFourhour}:${MinuteFormatted}:${secondsFormatted}`;
+    // constant for storing each part of the time together and properly formated
+    const timeFormatted = `${parts.hours}:${MinuteFormatted}:${secondsFormatted}`;
+    // constant for deciding if the time is in the morning or afternoon and displaying the right text depending
     const amPm = parts.isAm ? "AM" : "PM";
+    //constant to store what day it is to be used in switch below to highlight the correct day of the week
     const day = parts.day;
+    //switch to highlight the current day of the week depending on the value of the constant day
     switch (day) {
       case 0:
         const today0 = document.querySelector(".Sunday");
@@ -47,25 +55,16 @@ class DigitalClock {
         const today6 = document.querySelector(".Saturday");
         today6.style.color = "#00FF97";
     }
-    //if statment made to try and change between 12 and 24 hour formats does not currently work continue trying to fix it below
-    if (document.querySelector(".twelveHoursBtn").clicked == true) {
-      this.element.querySelector(".clock-time").textContent =
-        twelveHourTimeFormatted;
-    } else if (document.querySelector(".twentyFourHoursBtn").clicked == true) {
-      this.element.querySelector(".clock-time").textContent =
-        twentyFourHoursTimeFormatted;
-    } else {
-      this.element.querySelector(".clock-time").textContent =
-        "error something fucked up";
-    }
+    //actually changing the text value of the clocks time to the value of the constant time formated
+    this.element.querySelector(".clock-time").textContent = timeFormatted;
+    //changing the text value of the clocks am pm display to the value of the am pm constant
     this.element.querySelector(".clock-amPm").textContent = amPm;
   }
-
+  // function to get each of the time parts required to then use in the update function to enable the actual clock functionality also gets the day of the week.
   getTimeParts() {
     const now = new Date();
     return {
-      twelvehour: now.getHours() % 12 || 12,
-      twentyFourhour: now.getHours(),
+      hours: now.getHours() % 12 || 12,
       minute: now.getMinutes(),
       seconds: now.getSeconds(),
       isAm: now.getHours() < 12,
@@ -73,26 +72,9 @@ class DigitalClock {
     };
   }
 }
-
+//creating a constant that holds the value of the clock div
 const clockElement = document.querySelector(".clock");
+
 const clockObject = new DigitalClock(clockElement);
 
-console.log();
 clockObject.start();
-
-function timeFormatTwentyFourHours() {
-  const amPmDisplay = document.querySelector(".clock-amPm");
-  amPmDisplay.style.visibility = "hidden";
-  const clockRecenter = document.querySelector(".clock");
-  clockRecenter.style.left = "55%";
-  const clockRecenter2 = document.querySelector(".clock");
-  clockRecenter2.style.top = "50%";
-}
-function timeFormatTwelveHour() {
-  const amPmDisplay = document.querySelector(".clock-amPm");
-  amPmDisplay.style.visibility = "visible";
-  const clockRecenter = document.querySelector(".clock");
-  clockRecenter.style.left = "55%";
-  const clockRecenter2 = document.querySelector(".clock");
-  clockRecenter2.style.top = "50%";
-}
